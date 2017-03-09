b0fb1d00e27563922ab6961ff43482ee524644f7
Trinh.NCK <trinh.nck@gmail.com>
2017-03-09 11:36:40 +0700

    Using routerlink to link to hero detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,44 +1,48 @@
-import { NgModule }       from '@angular/core';
-import { BrowserModule }  from '@angular/platform-browser';
-import { FormsModule }    from '@angular/forms';
-import { RouterModule }   from '@angular/router';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
-import { AppComponent }        from './app.component';
-import { DashboardComponent }        from './dashboard.component';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
-import { HeroesComponent }     from './heroes.component';
-import { HeroService }         from './hero.service';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DashboardComponent,
-    HeroDetailComponent,
-    HeroesComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    RouterModule.forRoot([
-      {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full'
-      },
-      {
-        path: 'heroes',
-        component: HeroesComponent
-      },
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      },
-    ])
+	declarations: [
+		AppComponent,
+		DashboardComponent,
+		HeroDetailComponent,
+		HeroesComponent
+	],
+	imports: [
+		BrowserModule,
+		FormsModule,
+		RouterModule.forRoot([
+			{
+				path: '',
+				redirectTo: '/dashboard',
+				pathMatch: 'full'
+			},
+			{
+				path: 'heroes',
+				component: HeroesComponent
+			},
+			{
+				path: 'dashboard',
+				component: DashboardComponent
+			},
+			{
+				path: 'detail/:id',
+				component: HeroDetailComponent
+			},
+		])
 
-  ],
-  providers: [
-    HeroService
-  ],
-  bootstrap: [AppComponent]
+	],
+	providers: [
+		HeroService
+	],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
