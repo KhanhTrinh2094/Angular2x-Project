420e25b738a2a977a2e0f60385dbe8eeb5150263
Trinh.NCK <trinh.nck@gmail.com>
2017-03-09 10:06:36 +0700

    Using router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { RouterModule }   from '@angular/router';
+
+import { AppComponent }        from './app.component';
+import { DashboardComponent }        from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }     from './heroes.component';
-import { AppComponent } from './app.component';
 import { HeroService }         from './hero.service';
 
 @NgModule({
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroDetailComponent,
     HeroesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+    ])
+
   ],
   providers: [
     HeroService
